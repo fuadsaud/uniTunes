963223ebab0c1ad7e5394962cf0ce42192e4d343
Fuad Saud <fuadfsaud@gmail.com>
2016-06-17 13:44:00 -0300

    Seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!({
+  first_name: 'John',
+  last_name: 'Doe',
+  email: 'admin@unitunes.com',
+  password: 'admin123',
+  password_confirmation: 'admin123',
+  admin: true
+})
