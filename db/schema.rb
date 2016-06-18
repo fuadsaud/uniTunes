d62de15a251525622329d44e1b9296a87a3c21b0
Fuad Saud <fuadfsaud@gmail.com>
2016-06-18 14:10:39 -0300

    Add name to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618101101) do
+ActiveRecord::Schema.define(version: 20160618170610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160618101101) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",       null: false
   end
 
   create_table "load_funds_transactions", force: :cascade do |t|
