877795be59a2603b2d9404e34ecdeb14878a1a33
Fuad Saud <fuadfsaud@gmail.com>
2016-06-18 05:37:16 -0300

    Fix confirmable migrations

--- db/migrate/20160618070506_add_confirmable_to_devise.rb
+++ db/migrate/20160618070506_add_confirmable_to_devise.rb
@@ -3,20 +3,14 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
-
+    add_column :users, :unconfirmed_email, :string
     add_index :users, :confirmation_token, unique: true
 
-    # To avoid a short time window between running the migration and updating all existing
-    # users as confirmed, do the following
-    execute("UPDATE users SET confirmed_at = NOW()")
-    # All existing user accounts should be able to log in after this.
-    # Remind: Rails using SQLite as default. And SQLite has no such function :NOW.
-    # Use :date('now') instead of :NOW when using SQLite.
-    # => execute("UPDATE users SET confirmed_at = date('now')")
-    # Or => User.all.update_all confirmed_at: Time.now
+    execute('UPDATE users SET confirmed_at = NOW()')
   end
 
   def down
     remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    remove_columns :users, :unconfirmed_email
   end
 end
