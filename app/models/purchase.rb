e81abdb854fb8fa61b775d92b9328a52133d6a61
Fuad Saud <fuadfsaud@gmail.com>
2016-06-18 20:44:26 -0300

    Fix purchase relationships

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,10 @@
 class Purchase < ActiveRecord::Base
-  belongs_to :media
+  belongs_to :medium
   belongs_to :wallet
 
-  monetize :amount_centavos
+  validates_presence_of :medium, :wallet
+
+  monetize :amount_centavos, presence: true, numericality: {
+    greater_than_or_equal_to: 0
+  }
 end
