9693c3b8a456b2638f51c557a9fd504e4d5a4d9d
Fuad Saud <fuadfsaud@gmail.com>
2016-06-19 01:56:58 -0300

    Fix admin validation

--- app/validators/single_admin_validator.rb
+++ app/validators/single_admin_validator.rb
@@ -1,11 +1,17 @@
 class SingleAdminValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << 'an admin already exists' if value && admin_exists?
+    if value && admin_exists? && current_admin.id != record.id
+      record.errors[attribute] << 'an admin already exists' if value && admin_exists?
+    end
   end
 
   private
 
   def admin_exists?
-    User.exists?(admin: true)
+    current_admin.present?
+  end
+
+  def current_admin
+    User.admin
   end
 end
