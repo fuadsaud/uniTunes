22c64599c240e2108f3146f64ee6375489d451b6
Luiz Cunha <felipe.silvacunha@gmail.com>
2016-06-23 13:23:23 -0300

    Ajustada a rota de usuários

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def link_to_toggle_user_lock(user)
     link_to toggle_user_lock_label(currently_locked: user.access_locked?),
-      user_path(user.id, user: { locked: !user.access_locked? }), method: :put
+      admin_user_path(user.id, user: { locked: !user.access_locked? }), method: :put
   end
 
   def toggle_user_lock_label(currently_locked:)
